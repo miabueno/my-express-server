9ebc3334a4535b8fc9579ad112e2d505450c4a2a
Mia Bueno <mia.bueno101@gmail.com>
2021-01-31 22:36:00 +1100

    with requests to root URL, send response to app of Hello World in html formatting

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function(request, response) {
+    response.send("<h1>Hello World!</h1>");
+});
+
 // port 0 --> random free assign
 const server = app.listen(0, function() {
     // log what port server is hosted on
